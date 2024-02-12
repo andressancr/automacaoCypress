0eb7c2eec64d744504fd60de67c7bbfa5710c194
Andressa Rocha <andressancr@gmail.com>
2024-02-12 17:02:27 -0300

    Update
    update

--- cypress/e2e/testes/AutomacaoCypress.cy.js
+++ cypress/e2e/testes/AutomacaoCypress.cy.js
@@ -2,33 +2,35 @@
 
 import rgbHex from 'rgb-hex'
 
-describe('Acessar o site da Amazon e comprar o produto mais vendido', () => {
+describe('Acessar o site da Amazon e pesquisar os produtos mais vendidos'), () => {
 
-  it('Visitar o site da Amazon e adicionar o produto mais vendido no carrinho', () => {
+  it('Visitar o site da Amazon e pesquisar os produtos mais vendidos'), () => {
 
     cy.visit('https://amazon.com.br')
     cy.title().should('be.equal', 'Amazon.com.br | Tudo pra você, de A a Z.')
     cy.get('#twotabsearchtextbox').should('be.visible').type('Mais Vendidos{enter}')
     cy.title().should('be.equal', 'Amazon.com.br : Mais Vendidos')
-    cy.get('[data-asin="8543105692"] > .sg-col-inner > .s-widget-container > .s-card-container > .a-spacing-base > .a-spacing-small > .s-title-instructions-style > .a-size-mini > .a-link-normal > .a-size-base-plus').click()
-    cy.get('#add-to-cart-button').click()
 
-    cy.get('#nav-cart-count').should('be.visible').contains('1')
-    cy.get('#nav-cart-count').should('have.length', '1')
+  }
+  
+  it('Classificar produtos por preço do Menor para o Maior'), () => {    
 
-    //script validação cor
-    cy.get('#sc-buy-box-ptc-button').invoke('css', 'background-color')
-      .then((bgcolor) => {
-        expect(rgbHex(bgcolor)).to.eq('ffd814')
-      })
+    cy.contains('Classificar por').click()
+    cy.contains('do menor preço para o maior')
 
-    //script validação font
+  }
+    
+  it('Validar a cor do botão de classificação dos produtos'), () => { 
+
+    cy.get('.a-dropdown-label').invoke('css', 'background-color')
+      .then(bgcolor).expect(rgbHex(bgcolor)).to.eq('ffd814')
+  
+  }
+
+  it('Validar a fonte do botão de classificação dos produtos')
+
     cy.get('#sc-buy-box-ptc-button').should('have.css', 'font-family')
       .should('eq', '"Amazon Ember", Arial, sans-serif')
+  }
 
     //cy.get('.a-button-primary').invoke('css', 'background-color').then(bgcolor).expect(rgbHex(bgcolor)).to.eq('ef0b800')
-
-  })
-
-
-})
