379e1012a5caca00ba0d9855500cc622bb87b2f3
Andressa Rocha <andressancr@gmail.com>
2024-02-12 16:24:09 -0300

    Commit Alterações
    Commit

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,7 @@
 const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
+  projectId: 'anm6fi',
   reporter: "cypress-mochawesome-reporter",
   reporterOptions: {
     cypressMochawesomeReporterReporterOptions: {
